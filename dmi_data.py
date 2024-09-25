2500f65959d4f54dc0a36b37a625b11ef853b821
Martin Hallberg <martinhallberg@eivee.io>
2024-09-25 20:31:32 +0200

    Fixed parser

--- dmi_data.py
+++ dmi_data.py
@@ -14,9 +14,9 @@ def unzip_and_merge_dmi_obs_data(
     if n_files:
         files_to_parse = zip_file.infolist()[:n_files]
     
-    dfs = [pd.read_csv(zip_file.open(text_file.filename))
-        for text_file in files_to_parse
-        if text_file.filename.endswith(file_type)]
+    dfs = [read_file_in_zip(zip_file, file_)
+        for file_ in files_to_parse
+        if file_.filename.endswith(file_type)]
     
     df = pd.concat(dfs)
 
@@ -24,9 +24,9 @@ def unzip_and_merge_dmi_obs_data(
 
 
 def read_file_in_zip(
-        file_path,
+        zip_file,
         file_name,
-        n_rows=None
+        n_rows=None,
 ):
     
     list_parameters = [
@@ -48,25 +48,23 @@ def read_file_in_zip(
         "value"
     ]
 
-    with ZipFile(file_path) as z:
-        #for file_name in z.namelist():
-        with z.open(file_name) as f:
-        
-            print(f"Parsing file {file_name}")
-            list_rows = []
-            for line in f:
-                line_content = json.loads(line)["properties"]
-                
-                if (line_content["timeResolution"] == "hour") & (line_content["parameterId"] in list_parameters):
-
-                    list_values = [line_content[col] for col in list_columns_to_keep]
-
-                    list_rows.append(list_values)
-                
-                else:
-                    next
+    #with ZipFile(file_path) as z:
+    with zip_file.open(file_name) as f:
+        print(f"Parsing file {file_name}")
+        list_rows = []
+        for line in f:
+            line_content = json.loads(line)["properties"]
             
-            df = pd.DataFrame(list_rows, columns=list_columns_to_keep)
+            if (line_content["timeResolution"] == "hour") & (line_content["parameterId"] in list_parameters):
+
+                list_values = [line_content[col] for col in list_columns_to_keep]
+
+                list_rows.append(list_values)
+            
+            else:
+                next
+        
+        df = pd.DataFrame(list_rows, columns=list_columns_to_keep)
 
     return df
 
