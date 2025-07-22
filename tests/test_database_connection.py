44fffa6d2ce81f388ce3c2f1528ecbd7203d6b76
Martin Hallberg <88983960+MartinJHallberg@users.noreply.github.com>
2025-07-22 09:45:10 +0200

    Added test for database connection

---
+++ tests/test_database_connection.py
@@ -0,0 +1,26 @@
+import psycopg2
+from psycopg2 import OperationalError
+from dotenv import load_dotenv
+import os
+import pytest
+
+load_dotenv()
+
+DB_NAME = "weather"
+DB_USER = "weather_user"
+DB_PASSWORD = os.getenv("DB_WEATHER_PASSWORD")
+DB_HOST = "localhost"
+DB_PORT = "5432"
+
+def test_database_connection():
+    try:
+        conn = psycopg2.connect(
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT,
+        )
+        conn.close()
+    except OperationalError as e:
+        pytest.fail(f"Failed to connect to the PostgreSQL database: {e}") 
